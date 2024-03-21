efef589e323f272a2aa5b11e3cd42ba7fef73f1f
Alex Ament <alexcament@gmail.com>
2024-03-20 22:31:08 -0400

    create FileManager

---
+++ src/FileManager.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FileManager {
+
+    static final FileManager instance = new FileManager();
+
+    private FileManager() {}
+
+    /**
+     * @return the global FileManager instance
+     */
+    public static FileManager getInstance() {
+        return instance;
+    }
+
+    /**
+     * Creates a new directory
+     * @param name String the file's name
+     * @param path String the file's path
+     * @return boolean: true if the directory was created, false if it already exists
+     */
+    public boolean createDir(String name, String path) {
+
+        File f = new File(path + name);
+
+        return f.mkdir();
+    }
+}
