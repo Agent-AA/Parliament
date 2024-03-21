f6c93de3b5105c1611239f2e504aec585de07767
Alex Ament <alexcament@gmail.com>
2024-03-20 22:31:00 -0400

    add readInput function

--- src/ScreenWriter.java
+++ src/ScreenWriter.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ScreenWriter {
 
     static final ScreenWriter instance = new ScreenWriter();
@@ -11,6 +15,33 @@ public class ScreenWriter {
         System.out.println("|  ___/ _` | '__| | |/ _` | '_ ` _ \\ / _ \\ '_ \\| __|");
         System.out.println("| |  | (_| | |  | | | (_| | | | | | |  __/ | | | |");
         System.out.println("|_|   \\__,_|_|  |_|_|\\__,_|_| |_| |_|\\___|_| |_|\\__|");
+    }
+
+    /**
+     * Reads input from the terminal
+     * @param prompt String to prompt the user
+     * @return String input or null if no input.
+     */
+    public String readInput (String prompt) {
+
+        String input = null;
+
+        // Display the prompt
+        System.out.print(prompt);
+
+        // Enter data using BufferReader
+        BufferedReader reader = new BufferedReader(
+        new InputStreamReader(System.in));
+
+        try {
+            // Reading data using readLine
+            input = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Printing the read line
+        return input;
     }
 
     public static ScreenWriter getInstance() {
