c3f9ae6a5a116ea51c2a6f70d0b5b28245af1ccc
SIHS-Robotics <sihsrobotics@ignatius.edu>
2024-07-23 12:57:27 -0400

    start over project

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,5 @@
-import commands.CommandParser;
-import motionLib.Motion;
-import utils.ReaderWriter;
-
 public class Main {
     public static void main(String[] args) {
-
-        ReaderWriter.introScreen();
-
-        String session = ReaderWriter.readInput("\nEnter a session name: ");
-
-        Motion.setSession(session);
-        Motion.bufferAllMotions();
-
-        if (ReaderWriter.createDir(session, "./files/")) {
-            System.out.println("\n" + session + " session successfully created.");
-        } else {
-            System.out.println("\n" + session + " session opened.");
-        }
-
-        while (true) {
-            CommandParser.parseCommand(ReaderWriter.readInput("\n"+ session + "> "));
-        }
+        System.out.println("Hello, World!");
     }
 }
