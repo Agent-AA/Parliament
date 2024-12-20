aee4f2ace18a4d95cfb264307910b2c9fad86eec
Alexander Ament <118925546+Agent-AA@users.noreply.github.com>
2024-12-20 20:19:08 +0000

    Comment changes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path')
 const app = express();
 const port = 8000;
 
+//#region ----- Webpage and static file requests -----
 app.use(express.static(path.join(__dirname, '/webpage/player')));
 
 app.get('/', (req, res) => {
@@ -16,3 +17,4 @@ app.get('/admin', (req, res) => {
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
+//#endregion
