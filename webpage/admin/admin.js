35ba1a7dbafd56ced87864a023e442e831667464
Alexander Ament <118925546+Agent-AA@users.noreply.github.com>
2024-12-21 00:32:09 +0000

    fix admin update posting

--- webpage/admin/admin.js
+++ webpage/admin/admin.js
@@ -25,51 +25,109 @@ $(document).ready(() => {
             $("#speaker-time").text(data.speaker.timeRemaining);
             timePaused = data.speaker.timePaused;
 
-            $("#speaker-number").text("<strong>Speaker " + data.speaker.number + "</strong>");
-            
+            $("#speaker-number").text(
+                "<strong>Speaker " + data.speaker.number + "</strong>",
+            );
+
             $("#aff-count").text(data.speaker.aff.totalSpeeches);
-            $("#aff-last").text(data.speaker.aff.lastSpeaker + " (" + data.speaker.aff.speechTime + ")");
+            $("#aff-last").text(
+                data.speaker.aff.lastSpeaker +
+                    " (" +
+                    data.speaker.aff.speechTime +
+                    ")",
+            );
             $("aff-queue").empty();
-            for (let i = 0; i < data.speaker.speakingOrder.queue.aff.length; i++) {
-                $("#aff-queue").append("<li>" + data.speaker.speakingOrder.queue.aff[i] + "</li>");
+            for (
+                let i = 0;
+                i < data.speaker.speakingOrder.queue.aff.length;
+                i++
+            ) {
+                $("#aff-queue").append(
+                    "<li>" + data.speaker.speakingOrder.queue.aff[i] + "</li>",
+                );
             }
 
             $("#neg-count").text(data.speaker.neg.totalSpeeches);
-            $("#neg-last").text(data.speaker.neg.lastSpeaker + " (" + data.speaker.neg.speechTime + ")");
+            $("#neg-last").text(
+                data.speaker.neg.lastSpeaker +
+                    " (" +
+                    data.speaker.neg.speechTime +
+                    ")",
+            );
             $("neg-queue").empty();
-            for (let i = 0; i < data.speaker.speakingOrder.queue.neg.length; i++) {
-                $("#neg-queue").append("<li>" + data.speaker.speakingOrder.queue.neg[i] + "</li>");
+            for (
+                let i = 0;
+                i < data.speaker.speakingOrder.queue.neg.length;
+                i++
+            ) {
+                $("#neg-queue").append(
+                    "<li>" + data.speaker.speakingOrder.queue.neg[i] + "</li>",
+                );
             }
 
             $("#question-count").text(data.speaker.question.totalQuestions);
             $("#question-last").text(data.speaker.question.lastQuestioner);
             $("question-queue").empty();
             for (let i = 0; i < data.speaker.questionOrder.queue.length; i++) {
-                $("#question-queue").append("<li>" + data.speaker.questionOrder.queue[i] + "</li>");
+                $("#question-queue").append(
+                    "<li>" + data.speaker.questionOrder.queue[i] + "</li>",
+                );
             }
 
             $("#speaker-precedence").empty();
-            for (let i = 0; i < data.speaker.speakingOrder.precedence.length; i++) {
-                $("#speaker-precedence").append("<li>" + data.speaker.speakingOrder.precedence[i][0] + " " + data.speaker.speakingOrder.precedence[i][1] + "</li>");
+            for (
+                let i = 0;
+                i < data.speaker.speakingOrder.precedence.length;
+                i++
+            ) {
+                $("#speaker-precedence").append(
+                    "<li>" +
+                        data.speaker.speakingOrder.precedence[i][0] +
+                        " " +
+                        data.speaker.speakingOrder.precedence[i][1] +
+                        "</li>",
+                );
             }
 
             $("#speaker-recency").empty();
-            for (let i = 0; i < data.speaker.speakingOrder.recency.length; i++) {
-                $("#speaker-recency").append("<li>" + data.speaker.speakingOrder.recency[i][0] + "</li>");
+            for (
+                let i = 0;
+                i < data.speaker.speakingOrder.recency.length;
+                i++
+            ) {
+                $("#speaker-recency").append(
+                    "<li>" + data.speaker.speakingOrder.recency[i][0] + "</li>",
+                );
             }
 
             $("#question-precedence").empty();
-            for (let i = 0; i < data.speaker.questionOrder.precedence.length; i++) {
-                $("#question-precedence").append("<li>" + data.speaker.questionOrder.precedence[i][0] + " " + data.speaker.questionOrder.precedence[i][1] + "</li>");
+            for (
+                let i = 0;
+                i < data.speaker.questionOrder.precedence.length;
+                i++
+            ) {
+                $("#question-precedence").append(
+                    "<li>" +
+                        data.speaker.questionOrder.precedence[i][0] +
+                        " " +
+                        data.speaker.questionOrder.precedence[i][1] +
+                        "</li>",
+                );
             }
 
             $("#question-recency").empty();
-            for (let i = 0; i < data.speaker.questionOrder.recency.length; i++) {
-                $("#question-recency").append("<li>" + data.speaker.questionOrder.recency[i][0] + "</li>");
+            for (
+                let i = 0;
+                i < data.speaker.questionOrder.recency.length;
+                i++
+            ) {
+                $("#question-recency").append(
+                    "<li>" + data.speaker.questionOrder.recency[i][0] + "</li>",
+                );
             }
         }
     });
-})
+});
 
 // The calibrate function takes the text on the dashboard, parses it, and stores it locally so it can be
 // send to the server.
@@ -86,85 +144,85 @@ function calibrate() {
 
     affQueue = [];
     $("#aff-queue li").each((index, element) => {
-        affQueue.push(element.text());
+        affQueue.push(element.innerText);
     });
 
     negQueue = [];
     $("#neg-queue li").each((index, element) => {
-        negQueue.push(element.text());
+        negQueue.push(element.innerText);
     });
 
     questionQueue = [];
     $("#question-queue li").each((index, element) => {
-        questionQueue.push(element.text());
+        questionQueue.push(element.innerText);
     });
-    
+
     speakerPrecedence = [];
     $("#speaker-precedence li").each((index, element) => {
-        speakerPrecedence.push(element.text().split(" "));
+        speakerPrecedence.push(element.innerText.split(" "));
     });
 
     speakerRecency = [];
     $("#speaker-recency li").each((index, element) => {
-        speakerRecency.push(element.text());
+        speakerRecency.push(element.innerText);
     });
 
     questionPrecedence = [];
     $("#question-precedence li").each((index, element) => {
-        questionPrecedence.push(element.text().split(" "));
+        questionPrecedence.push(element.innerText.split(" "));
     });
 
     questionRecency = [];
     $("#question-recency li").each((index, element) => {
-        questionRecency.push(element.text());
+        questionRecency.push(element.innerText);
     });
 }
 
 // The quintessential update function sends all of our data to the server.
 function sendUpdate() {
     const data = {
-        "motion" : $("#motion-title").val(),
+        "motion" : $("#motion-title").text(),
         "speaker" : {
-            "name" : $("#speaker-name").val(),
-            "disposition" : $("#speaker-disposition").val(),
-            "timeRemaining" : $("#speaker-time").val(),
-            "number" : $("#speaker-number").val(),
-            "timePaused" : timePaused,
-            "aff" : {
-                "totalSpeeches" : affTotalSpeeches,
-                "lastSpeaker" : affLastSpeaker,
-                "speechTime" : affSpeechTime
+            "name" : $("#speaker-name").text(),
+            "disposition": $("#speaker-disposition").text(),
+            "timeRemaining": $("#speaker-time").text(),
+            "number": $("#speaker-number").text(),
+            "timePaused": timePaused,
+            "aff": {
+                "totalSpeeches": affTotalSpeeches,
+                "lastSpeaker": affLastSpeaker,
+                "speechTime": affSpeechTime,
             },
-            "neg" : {
-                "totalSpeeches" : negTotalSpeeches,
-                "lastSpeaker" : negLastSpeaker,
-                "speechTime" : negSpeechTime
+            "neg": {
+                "totalSpeeches": negTotalSpeeches,
+                "lastSpeaker": negLastSpeaker,
+                "speechTime": negSpeechTime,
             },
-            "question" : {
-                "totalQuestions" : questionCount,
-                "lastQuestioner" : lastQuestioner
+            "question": {
+                "totalQuestions": questionCount,
+                "lastQuestioner": lastQuestioner,
             },
-            "speakingOrder" : {
-                "precedence" : speakerPrecedence,
-                "recency" : speakerRecency,
+            "speakingOrder": {
+                "precedence": speakerPrecedence,
+                "recency": speakerRecency,
                 "queue": {
-                    "aff" : affQueue,
-                    "neg" : negQueue
-                }
+                    "aff": affQueue,
+                    "neg": negQueue,
+                },
             },
-            "questionOrder" : {
-                "precedence" : questionPrecedence,
-                "recency" : questionRecency,
-                "queue" : questionQueue
-            }
-        }
-    }
+            "questionOrder": {
+                "precedence": questionPrecedence,
+                "recency": questionRecency,
+                "queue": questionQueue,
+            },
+        },
+    };
+    console.log(data);
+    $.post("/update", data);
 }
 
 // When I press ctrl + u, I want to call the calibrate() and then sendUpdate() functions.
-$(document).keydown((event) => {
-    if (event.ctrlKey && event.key === "u") {
-        calibrate();
-        sendUpdate();
-    }
+$("#update-motion-button").click(() => {
+    calibrate();
+    sendUpdate();
 });
